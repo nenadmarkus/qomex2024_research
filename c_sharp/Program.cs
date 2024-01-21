8e7ed539df04440f850a14fac1dca8aacec37cdd
Nenad <nenad@m1mac>
2024-01-21 15:22:36 +0100

    add measurements

--- c_sharp/Program.cs
+++ c_sharp/Program.cs
@@ -37,8 +37,70 @@ class Program
         });
     }
 
+    static (double, double) Test4()
+    {
+        var sw = new System.Diagnostics.Stopwatch();
+        var com = new LlamaCppCom();
+
+        //
+        double t0 = 0.0;
+        int nchunks = 0;
+
+        com.OnResponseChunk = (string chunk) => {
+            if (nchunks == 0)
+            {
+                // this is the time to first chunk
+                t0 = sw.Elapsed.TotalSeconds;
+
+                // restart the timer - we measure how fast the stream of chunks comes next
+                sw = System.Diagnostics.Stopwatch.StartNew();
+            }
+            ++nchunks;
+            Console.Write(chunk);
+        };
+
+        sw.Start();
+        com.Communicate(
+            "Building a website can be done in 10 simple steps:",
+            512, null
+        );
+
+        // we had an error in server returning 0 chunks!
+        // fix with this hack
+        if (nchunks == 0)
+            return Test4();
+
+        t0 = 1000*t0;
+        double tpc = 1000 * sw.Elapsed.TotalSeconds / nchunks;
+
+        Console.WriteLine("\n");
+        Console.WriteLine($"* time to first chunk: {(int)t0} [ms]");
+        Console.WriteLine($"* time per chunk: {tpc} [ms]");
+
+        return (t0, tpc);
+    }
+
+    static void Test5()
+    {
+        int N = 8;
+        double t0_sum = 0.0, tpc_sum = 0.0;
+
+        Test4(); // warm up run
+
+        for (int i=0; i<N; ++i)
+        {
+            var (t0, tpc) = Test4();
+            t0_sum += t0;
+            tpc_sum += tpc;
+        }
+
+        Console.WriteLine("\n");
+        Console.WriteLine($"* average time to first chunk: {(int)(t0_sum/N)} [ms]");
+        Console.WriteLine($"* average time per chunk: {(int)(tpc_sum/N)} [ms]");
+    }
+
     static void Main(string[] args)
     {
-        Test3();
+        Test5();
     }
 }
